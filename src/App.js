f621029e855a37acb666e12d67cc7c861687f167
sharanabasappadhammur <108877043+sharanabasappadhammur@users.noreply.github.com>
2024-04-16 15:21:09 +0530

    1212

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 function App() {
-  // if ("serviceWorker" in navigator) {
-  //   navigator.serviceWorker.getRegistrations().then((registrations) => {
-  //     registrations.forEach((registration) => {
-  //       registration.unregister();
-  //     });
-  //   });
-  // }
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+      registrations.forEach((registration) => {
+        registration.unregister();
+      });
+    });
+  }
 
   return <div className="App">1</div>;
 }
