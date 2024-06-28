304c9051ee6dc6aac0e4f9de4d7366e5d8b1a514
sharanabasappadhammur <108877043+sharanabasappadhammur@users.noreply.github.com>
2024-06-28 15:55:19 +0530

    dssdsd

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
-import "./App.css";
+// import "./App.css";
+// import React, { useState, useRef, useEffect } from "react";
+// import ChartComponent from "./ChartComponent";
 
-function App() {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.getRegistrations().then((registrations) => {
-      registrations.forEach((registration) => {
-        console.log("111");
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const [count2, setCount2] = useState(0);
+//   const prevCountRef = useRef();
 
-        registration.unregister();
-      });
-    });
-  }
+//   useEffect(() => {
+//     prevCountRef.current = count;
+//     setCount2(count);
+//     console.log(prevCountRef);
+//   }, [count]);
 
-  return <div className="App">10</div>;
-}
+//   console.log("test");
+
+//   return (
+//     <div>
+//       <div>
+//         <ChartComponent />  
+//         {/* <h1>Current Count: {count}</h1>
+//         <h1>Current Count2: {count2}</h1>
+//         <h2>Previous Count: {prevCountRef.current}</h2>
+//         <button onClick={() => setCount(count + 1)}>Increment</button> */}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+// App.js
+import React from 'react';
+import useMetaTags from './UseMetaTags';
+
+const App = () => {
+  useMetaTags({
+    title: 'Your Website Title',
+    description: 'A brief description of your website',
+    image: 'https://coffeeweb.s3.ap-south-1.amazonaws.com/devtestlogo.png',
+    url: 'https://silly-sunburst-8de3f8.netlify.app/',
+  });
+
+  return (
+    <div>
+      <h1>Welcome to Your Website</h1>
+      <p>Some content goes here...</p>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/UseMetaTags.js
@@ -0,0 +1,36 @@
+// useMetaTags.js
+import { useEffect } from 'react';
+
+const useMetaTags = ({ title, description, image, url }) => {
+  useEffect(() => {
+    if (title) {
+      document.title = title;
+      setMetaTag('property', 'og:title', title);
+      setMetaTag('name', 'twitter:title', title);
+    }
+    if (description) {
+      setMetaTag('property', 'og:description', description);
+      setMetaTag('name', 'twitter:description', description);
+    }
+    if (image) {
+      setMetaTag('property', 'og:image', image);
+      setMetaTag('name', 'twitter:image', image);
+    }
+    if (url) {
+      setMetaTag('property', 'og:url', url);
+    }
+    setMetaTag('name', 'twitter:card', 'summary_large_image');
+  }, [title, description, image, url]);
+
+  const setMetaTag = (attrName, attrValue, content) => {
+    let element = document.querySelector(`meta[${attrName}='${attrValue}']`);
+    if (!element) {
+      element = document.createElement('meta');
+      element.setAttribute(attrName, attrValue);
+      document.head.appendChild(element);
+    }
+    element.setAttribute('content', content);
+  };
+};
+
+export default useMetaTags;
