6564ab7010e76d741622bd194675cf012a182873
sharanabasappadhammur <108877043+sharanabasappadhammur@users.noreply.github.com>
2025-02-12 17:25:52 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,37 +1,6 @@
-// import "./App.css";
-// import React, { useState, useRef, useEffect } from "react";
-// import ChartComponent from "./ChartComponent";
-
-// function App() {
-//   const [count, setCount] = useState(0);
-//   const [count2, setCount2] = useState(0);
-//   const prevCountRef = useRef();
-
-//   useEffect(() => {
-//     prevCountRef.current = count;
-//     setCount2(count);
-//     console.log(prevCountRef);
-//   }, [count]);
-
-//   console.log("test");
-
-//   return (
-//     <div>
-//       <div>
-//         <ChartComponent />
-//         {/* <h1>Current Count: {count}</h1>
-//         <h1>Current Count2: {count2}</h1>
-//         <h2>Previous Count: {prevCountRef.current}</h2>
-//         <button onClick={() => setCount(count + 1)}>Increment</button> */}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-// App.js
 import React from "react";
 import useMetaTags from "./UseMetaTags";
+import { Helmet } from "react-helmet";
 
 const App = () => {
   useMetaTags({
@@ -43,7 +12,18 @@ const App = () => {
 
   return (
     <div>
-      <h1>Welcome to Your Website.....</h1>
+      <Helmet>
+        <meta property="og:title" content={"Loading..."} />
+        <meta property="og:description" content={"Loading..."} />
+        <meta property="og:image" content={"/default-image.jpg"} />
+        <meta
+          property="og:url"
+          content="https://coffeeweb.s3.ap-south-1.amazonaws.com/coffeenewsfeeds/Uptrend.png"
+        />
+        <meta property="og:type" content="article" />
+        <title>{"news.title"}</title>
+      </Helmet>
+      <h1>Welcome to Your Website...............</h1>
       <p>Some content goes here...</p>
     </div>
   );
