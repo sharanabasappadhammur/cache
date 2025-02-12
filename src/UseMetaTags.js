3c2d3f6be37420d5fc1fdcc9237ee5a0269f5c8f
sharanabasappadhammur <108877043+sharanabasappadhammur@users.noreply.github.com>
2025-02-12 17:41:20 +0530

    sasa

--- src/UseMetaTags.js
+++ src/UseMetaTags.js
@@ -1,35 +1,35 @@
 // useMetaTags.js
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 const useMetaTags = ({ title, description, image, url }) => {
   useEffect(() => {
     if (title) {
       document.title = title;
-      setMetaTag('property', 'og:title', title);
-      setMetaTag('name', 'twitter:title', title);
+      setMetaTag("property", "og:title", title);
+      setMetaTag("name", "twitter:title", title);
     }
     if (description) {
-      setMetaTag('property', 'og:description', description);
-      setMetaTag('name', 'twitter:description', description);
+      setMetaTag("property", "og:description", description);
+      setMetaTag("name", "twitter:description", description);
     }
     if (image) {
-      setMetaTag('property', 'og:image', image);
-      setMetaTag('name', 'twitter:image', image);
+      setMetaTag("property", "og:image", image);
+      setMetaTag("name", "twitter:image", image);
     }
     if (url) {
-      setMetaTag('property', 'og:url', url);
+      setMetaTag("property", "og:url", url);
     }
-    setMetaTag('name', 'twitter:card', 'summary_large_image');
+    setMetaTag("name", "twitter:card", "summary_large_image");
   }, [title, description, image, url]);
 
   const setMetaTag = (attrName, attrValue, content) => {
     let element = document.querySelector(`meta[${attrName}='${attrValue}']`);
     if (!element) {
-      element = document.createElement('meta');
+      element = document.createElement("meta");
       element.setAttribute(attrName, attrValue);
       document.head.appendChild(element);
     }
-    element.setAttribute('content', content);
+    element.setAttribute("content", content);
   };
 };
 
